284252155aa62613f5abb9482f01341c3e85ee1e
Taeyong Kim <93954052+TYongK@users.noreply.github.com>
2023-06-19 10:01:58 -0500

    Update PDinCubeSampling.cs

--- PDinCubeSampling.cs
+++ PDinCubeSampling.cs
@@ -110,3 +110,16 @@ public void PoissonDiskCube(Box cube, double radi, int iterTest, out List<Point3
 
     return PPoints;
   }
+
+  public double NextGaussian(Random r, double mu = 0, double sigma = 1)
+  {
+    var u1 = r.NextDouble();
+    var u2 = r.NextDouble();
+
+    var rand_std_normal = Math.Sqrt(-2.0 * Math.Log(u1)) *
+      Math.Sin(2.0 * Math.PI * u2);
+
+    var rand_normal = mu + sigma * rand_std_normal;
+
+    return rand_normal;
+  }
